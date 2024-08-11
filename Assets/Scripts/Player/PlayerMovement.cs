090bfcf0415e337de83cf61b589d67dae6addeca
Ivan Smirnov <vano-smirnov@bk.ru>
2024-08-11 19:42:37 +0300

    added player prefab and player movement component

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,19 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-[]
+[RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	[SerializeField] private float _playerSpeed = 10;
+
+    private Rigidbody2D _player;
+	private Vector2 _movementVector = Vector2.zero;
+
+
+	void Start()
     {
-        
+        _player = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
-    }
+		if (_movementVector == Vector2.zero)
+			return;
+
+		_player.AddForce(_movementVector * _playerSpeed);
+	}
+
+	private void OnMove(InputValue movementValue)
+	{
+		_movementVector = movementValue.Get<Vector2>();
+		Debug.Log(_movementVector);
+	}
 }
